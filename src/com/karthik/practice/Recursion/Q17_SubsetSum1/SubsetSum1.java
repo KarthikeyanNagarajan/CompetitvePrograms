999da81c721b1860ff0d6577f6692dab03068774
Karthikeyan <karthik97nagaraj@gmail.com>
2024-06-12 23:55:17 +0530

    Commit on 12th June

---
+++ src/com/karthik/practice/Recursion/Q17_SubsetSum1/SubsetSum1.java
@@ -0,0 +1,12 @@
+package com.karthik.practice.Recursion.Q17_SubsetSum1;
+
+public class SubsetSum1
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/karthik/practice/Recursion/Q18_SubsetSum2/SubsetSum2.java
@@ -0,0 +1,12 @@
+package com.karthik.practice.Recursion.Q18_SubsetSum2;
+
+public class SubsetSum2
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}

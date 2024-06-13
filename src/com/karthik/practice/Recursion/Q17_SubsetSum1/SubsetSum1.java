da6becaea33544ecdb8415135d39c915a4f04d60
Karthikeyan <karthik97nagaraj@gmail.com>
2024-06-13 20:56:53 +0530

    Commit on 13th June

--- src/com/karthik/practice/Recursion/Q17_SubsetSum1/SubsetSum1.java
+++ src/com/karthik/practice/Recursion/Q17_SubsetSum1/SubsetSum1.java
@@ -1,12 +1,32 @@
 package com.karthik.practice.Recursion.Q17_SubsetSum1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class SubsetSum1
 {
+	public static void func(int ind, int sum, List<Integer> arr, int n, List<Integer> sumSubset)
+	{
+		if (ind == n)
+		{
+			sumSubset.add(sum);
+			return;
+		}
+
+		func(ind + 1, sum + arr.get(ind), arr, n, sumSubset);
+		func(ind + 1, sum, arr, n, sumSubset);
+	}
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		List<Integer> arr = Arrays.asList(2, 3);
+		int n = 2;
+		List<Integer> sumSubset = new ArrayList<>();
+		func(0, 0, arr, n, sumSubset);
+		Collections.sort(sumSubset);
+		System.out.println(sumSubset);
 	}
 
 }
--- src/com/karthik/practice/Recursion/Q18_SubsetSum2/SubsetSum2.java
+++ src/com/karthik/practice/Recursion/Q18_SubsetSum2/SubsetSum2.java
@@ -1,12 +1,30 @@
 package com.karthik.practice.Recursion.Q18_SubsetSum2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubsetSum2
 {
+	public static void func(int ind, List<Integer> arr, List<Integer> ds, List<List<Integer>> ans)
+	{
+		ans.add(new ArrayList<>(ds));
+		for (int i = ind; i < arr.size(); i++)
+		{
+			if (i != ind && arr.get(i) == arr.get(i - 1))
+				continue;
+			ds.add(arr.get(i));
+			func(i + 1, arr, ds, ans);
+			ds.remove(ds.size() - 1);
+		}
+	}
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		List<Integer> arr = Arrays.asList(1, 2, 2);
+		List<List<Integer>> ans = new ArrayList<>();
+		func(0, arr, new ArrayList<>(), ans);
+		System.out.println(ans);
 	}
 
 }
